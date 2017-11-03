e79e3fbdee9eac718367af69049660b9de4de303
elijahducote <validpupflower@gmail.com>
2017-11-03 17:39:48 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const { Client } = require('discord.js');
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+
+   console.log('${client.user.username} is ready');
+   client.user.setGame(game: 'o n ${bot.guilds.size} s e r v e r( s)');
+
+});
+
+
+client.login(process.env.APP_TOKEN);
