da31843431a1433c318acfa3bb4cdf9331769461
elijahducote <validpupflower@gmail.com>
2017-11-04 09:21:45 +0000

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
 
    console.log('${client.user.username} is ready');
-   client.user.setGame('on ${bot.guilds.size} server(s)');
-   client.user.setGame('http://dumbkids.xyz');
-   
+   client.user.setGame('on ${bot.guilds.size} server(s)', 'https://dumbkids.xyz')
 
 });
 
