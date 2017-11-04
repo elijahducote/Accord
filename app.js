f865c2a6e6edef2c0234b3e60bf2e1de21f2667d
elijahducote <validpupflower@gmail.com>
2017-11-04 10:45:06 +0000

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
 
    console.log('${client.user.username} is ready');
    client.user.setPresence({ game: { name: 'on 1 server(s)', type: 0 } });
-   client.user.setPresence({ game: { name: 'https://dumbkids.xyz', type: 1 } })
+   client.user.setGame('on 1 server(s)', 'https://dumbkids.xyz')
 
 });
 
